5eeae197dc06864198b073fecc7319a751ee1646
Kard9876 <dark9876king@gmail.com>
2024-03-14 21:43:29 +0000

    Engine correction

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -19,6 +19,10 @@
 #include <thread>
 #include <mutex>
 
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
 ///////////////////////////////////////////////////////// main objects. temporario, depois vai sair daqui
 InputHandler inputHandler;
 XmlParser xmlParser;
